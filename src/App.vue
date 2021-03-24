a0c8de736c7ef98c199e6e444146b7b6c3a9b3ff
Sergey Zubkov <ksevelyar@gmail.com>
2021-03-24 20:30:10 +0300

    use Iosevka as main font

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,18 @@ body {
   margin: 0;
 }
 
+@font-face {
+  font-family: 'Iosevka';
+  font-display: swap;
+  font-weight: 500;
+  font-stretch: normal;
+  font-style: normal;
+  src: url('~@/assets/fonts/iosevka-medium.woff2') format('woff2');
+}
+
 #app {
+  font-family: 'Iosevka';
+
   min-height: 100vh;
   display: flex;
   justify-content: center;
