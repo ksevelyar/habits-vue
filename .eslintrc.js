5bb1607584f29b9cf32f9e6a7a46b2f048a2c237
Sergey Zubkov <ksevelyar@gmail.com>
2022-01-10 18:36:46 +0300

    adjust eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,15 +4,18 @@ module.exports = {
     node: true
   },
   extends: [
-    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
     '@vue/standard'
   ],
   parserOptions: {
+    ecmaVersion: 2021,
     parser: 'babel-eslint'
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/component-name-in-template-casing': ['error', 'PascalCase']
   },
   overrides: [
     {
