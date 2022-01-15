5a90c35acffc297cd4d49339db35af08b40aed4d
Sergey Zubkov <ksevelyar@gmail.com>
2022-01-15 05:40:28 +0300

    initial user view and client

--- src/router/index.js
+++ src/router/index.js
@@ -6,15 +6,12 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/:handle',
+    name: 'User',
+    component: () => import(/* webpackChunkName: "user" */ '../views/User.vue')
   }
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
 ]
 
 const router = createRouter({
