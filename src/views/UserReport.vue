42976cc7f282b06aab43fd7752363787028eba8a
Sergey Zubkov <ksevelyar@protonmail.com>
2022-04-19 20:18:46 +0300

    typecast report inputs to numbers, serve dev env to local network

--- src/views/UserReport.vue
+++ src/views/UserReport.vue
@@ -1,13 +1,48 @@
 <template lang="pug">
 .report
-  input.report__input.report__date(v-model="report.date" type="text" placeholder="date")
-  input.report__input.report__weight(v-model="report.weight" type="number" placeholder="weight")
-  input.report__input.report__stepper(v-model="report.stepper" type="number" placeholder="stepper")
-  input.report__input.report__dumbbell_sets(v-model="report.dumbbell_sets" type="number" placeholder="dumbbels sets")
-  input.report__input.report__pullups(v-model="report.pullups" type="number" placeholder="pullups")
-  input.report__input.report__protein(v-model="report.protein" type="number" placeholder="protein")
-  input.report__input.report__fat(v-model="report.fat" type="number" placeholder="fat")
-  input.report__input.report__carbs(v-model="report.carbs" type="number" placeholder="carbs")
+  input.report__input.report__date(
+    v-model.number="report.date"
+    type="text"
+    placeholder="date"
+  )
+  input.report__input.report__weight(
+    v-model.number="report.weight"
+    type="number"
+    placeholder="weight"
+  )
+  input.report__input.report__steps(
+    v-model.number="report.steps"
+    type="number"
+    placeholder="steps"
+  )
+  input.report__input.report__stepper(
+    v-model.number="report.stepper"
+    type="number"
+    placeholder="stepper"
+  )
+  input.report__input.report__dumbbell_sets(
+    v-model.number="report.dumbbell_sets"
+    type="number"
+    placeholder="dumbbels sets"
+  )
+  input.report__input.report__pullups(
+    v-model.number="report.pullups" type="number" placeholder="pullups"
+  )
+  input.report__input.report__protein(
+    v-model.number="report.protein"
+    type="number"
+    placeholder="protein"
+  )
+  input.report__input.report__fat(
+    v-model.number="report.fat"
+    type="number"
+    placeholder="fat"
+  )
+  input.report__input.report__carbs(
+    v-model.number="report.carbs"
+    type="number"
+    placeholder="carbs"
+  )
 </template>
 
 <script setup>
@@ -16,6 +51,7 @@ import { reactive } from 'vue'
 const report = reactive({
   date: (new Date).toLocaleDateString(),
   weight: null,
+  steps: null,
   stepper: null,
   dumbbell_sets: null,
   pullups: null,
@@ -34,9 +70,10 @@ const report = reactive({
   height: 100vh
 
 .report__input
-  margin-top: 1em
+  margin-top: 2vh
   box-sizing: border-box
   width: 80vw
   max-width: 40ch
-  height: 3em
+  height: 6vh
+  padding: 0px 1ch
 </style>
