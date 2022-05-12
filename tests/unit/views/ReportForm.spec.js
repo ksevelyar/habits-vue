bcd18019b4b509f81568c7ad1200abfee790a6d0
Sergey Zubkov <ksevelyar@protonmail.com>
2022-05-12 17:18:59 +0300

    extract report input

--- tests/unit/views/ReportForm.spec.js
+++ tests/unit/views/ReportForm.spec.js
@@ -4,7 +4,7 @@ import { expect, test } from 'vitest';
 
 test('renders form', async () => {
   const form = mount(ReportForm)
-  const date = form.get('.report-form__date')
+  const date = form.get('.report-input__date')
 
   const kebabCase = 'fr-CA'
   expect(date.element.value).toBe((new Date).toLocaleDateString(kebabCase))
