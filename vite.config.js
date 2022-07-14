746a71d3977bc879fe9b7db3a6ba8dae16fd7b5d
Sergey Zubkov <ksevelyar@protonmail.com>
2022-07-14 14:14:13 +0300

    migrate to vite 3

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    port: 3000
+  },
   resolve: {
     alias: { '@': path.resolve(__dirname, './src') },
   },
