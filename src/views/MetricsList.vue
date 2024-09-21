7b7e573eb1f328dbb3cfb8724a1efe02f63819ec
Sergey Zubkov <ksevelyar@protonmail.com>
2024-09-21 22:11:54 +0300

    switch date in metrics form

--- src/views/MetricsList.vue
+++ src/views/MetricsList.vue
@@ -4,13 +4,13 @@ table.metrics-history
     th.metrics-history__head date
     th.metrics-history__head(v-for="chain in chainsHistory") {{ chain.name }}
 
-  tr(v-for="(metrics, date) in metricsHistory")
+  tr.metrics-history__report(v-for="(metrics, date) in metricsHistory" @click="getForm(date)")
     td {{ date }}
     td(v-for="chain in chainsHistory")
       | {{ historyValue(metrics, chain) }}
 
 
-MetricForm(v-for="metric in metrics" :metric="metric" :date="date")
+MetricForm(v-for="(metric, ind) in metrics" v-model="metrics[ind]" :date="date")
 </template>
 
 <script setup>
@@ -19,16 +19,14 @@ import metricClient from '@/api/metric-client'
 import router from '@/router'
 import MetricForm from '@/components/MetricForm.vue'
 
-// generate metrics from chains
 const metrics = ref([])
 const metricsHistory = ref([])
 const chainsHistory = ref([])
 
-const [date,] = new Date().toISOString().split('T')
-
-const getList = async () => {
-  metrics.value = (await metricClient.index(date))
+const getForm = async (date) => {
+  metrics.value = (await metricClient.index(date)).map(x => ({ ...x, date }))
 }
+
 const getHistory = async () => {
   const {metrics, chains} = (await metricClient.history())
 
@@ -38,7 +36,10 @@ const getHistory = async () => {
 
 const historyValue = (metrics, chain) => metrics[chain.id]?.value
 
-getList()
+const loadMetric = metric => console.log(metric)
+
+const [date,] = new Date().toISOString().split('T')
+getForm(date)
 getHistory()
 
 // set interval for seconds until next date
@@ -50,4 +51,7 @@ getHistory()
   background: #c7c7c7
   border: 1px solid #aaa
   padding: 5px
+
+.metrics-history__report
+  cursor: pointer
 </style>
